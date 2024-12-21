5c9aac24dd11bfffaa66f04036c1e9642358bc9f
Goodman <60833584+CCGoodman@users.noreply.github.com>
2024-12-21 21:44:00 +0100

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import path from 'path';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    }
+  },
+  base: process.env.ELECTRON ? './' : '/',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    },
+    sourcemap: true
+  },
+  server: {
+    port: 5173
+  }
+});
